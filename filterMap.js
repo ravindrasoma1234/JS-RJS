184d792b47cb506efc9e5ca11c8f49a05e341987
ravindrasoma1234 <72332287+ravindrasoma1234@users.noreply.github.com>
2024-11-19 14:57:36 +0530

    added MapFilter

---
+++ filterMap.js
@@ -0,0 +1,61 @@
+const myNum = [1, 2, 3, 4, 5, 6, 7, 8,9,10]
+
+myNum.filter((num) =>{
+   // console.log(num)
+})
+
+const numFilter = myNum.filter((num) =>{
+    if(num > 5) return num;
+})
+//console.log(numFilter);
+
+const newArray = []
+
+myNum.forEach((num) =>{
+    const newValues = num
+    myNum.push(newValues)
+   // console.log(newValues)
+})
+
+const newArray1 = [10,11,12,13,14,15,16,17,18]
+
+newArray1.forEach((item) =>{
+    myNum.push(item)
+});
+
+// console.log(newArray1);
+// console.log(myNum);
+
+
+// ----------------------------------------------------------------
+
+// oldArray = ["a", "b", "c", "d", "e", "f"]
+// nweArray2 = ["x,y", "z"]
+
+
+const book =[
+    {
+        title : "Book One", genre : "Fiction", publish : 1981, edition : 2004
+    },
+    {
+        title : "Book Two", genre : "Non-Fiction", publisher :1865, edition : 2001
+    },
+    {
+        title : "Book Three", genre : "historical", publisher :1986, edition :2000
+    },
+    {
+        title : "Book Four", genre : "science", publisher :1998, edition :2014
+    },
+    {
+        title : "Book Five", genre : "finance", publisher :1879 , edition :2001,
+    
+    }
+]
+
+
+aaaaaaaaaaaaaaaaaaaaaa
+
+asdafsavc
+
+
+
